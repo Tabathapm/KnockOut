23d09cfd5968b4e794e9e0cf94ccb8e358ee77c7
Eri-random <brenherrera@alumno.unlam.edu.ar>
2022-09-26 15:28:03 -0300

    agregando protocolo TLSv1.2

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioMailImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioMailImpl.java
@@ -58,6 +58,7 @@ public class ServicioMailImpl implements ServicioMail {
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.user", username);
         props.put("mail.smtp.password", password);
