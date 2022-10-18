0299bc1509a63755dbd9074e1ed3686c0302ae9e
Tabathapm <tabathapm@gmail.com>
2022-10-18 20:40:52 -0300

    mi perfil

---
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorUsuario.java
@@ -0,0 +1,40 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import ar.edu.unlam.tallerweb1.servicios.ServicioUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ControladorUsuario {
+
+    private ServicioUsuario servicioUsuario;
+
+    @Autowired
+    public ControladorUsuario(ServicioUsuario servicioUsuario){
+        this.servicioUsuario = servicioUsuario;
+    }
+
+    @RequestMapping("/miPerfil")
+    private ModelAndView irMiPerfil(HttpServletRequest request){
+
+        Integer usuario_id = (Integer) request.getSession().getAttribute("idUsuario");
+
+        if(usuario_id == null){
+            return new ModelAndView("redirect:/login");
+        }
+//      --------------------------------
+        ModelMap model = new ModelMap();
+//      --------------------------------
+        Usuario usuario = servicioUsuario.buscarPorID(usuario_id);
+
+        model.put("usuario", usuario);
+
+        return new ModelAndView("miPerfil", model);
+    }
+}
